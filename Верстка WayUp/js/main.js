e567d779a676340bb593df25b78169452f3acc0d
Danyatester <51127145+Danyatester@users.noreply.github.com>
2019-05-28 18:14:46 +0300

    Add files via upload

---
+++ Верстка WayUp/js/main.js
@@ -0,0 +1,120 @@
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.elfel').css(
+		'transform',
+	    'translate(-' + x * 60 + 'px, -' + y * 60 + 'px)'
+    );        
+});
+
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.kriv').css(
+		'transform',
+	    'translate(+' + x * 20 + 'px, -' + y * 20 + 'px)'
+    );        
+});
+
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.plyes').css(
+		'transform',
+	    'translate(+' + x * 30 + 'px, -' + y * 35 + 'px)'
+    );        
+});
+
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.lin').css(
+		'transform',
+	    'translate(-' + x * 20 + 'px, -' + y * 60 + 'px)'
+    );        
+});
+
+
+
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.trel').css(
+		'transform',
+	    'translate(+' + x * 50 + 'px, -' + y * 40 + 'px)'
+    );        
+});
+
+
+
+
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.cloi').css(
+		'transform',
+	    'translate(+' + x * 10 + 'px, -' + y * 10 + 'px)'
+    );        
+});
+
+
+
+
+
+$('.first').on('mousemove' , (e) =>{
+	const x = e.pageX / $(window).width();
+	const y = e.pageY / $(window).height();
+
+	$('.body').css(
+		'transform',
+	    'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
+    );    
+
+
+	$('.chel').css(
+		'transform',
+	    'translate(-' + x * 20 + 'px, -' + y * 0 + 'px)'
+    );        
+});
